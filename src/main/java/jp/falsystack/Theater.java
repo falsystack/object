e398953452120f9731b722268d65156aa1ed8810
falsystack <falsystack@gmail.com>
2023-03-15 21:32:07 +0900

    feat(chap01): add Theater and TicketSeller

--- src/main/java/jp/falsystack/Theater.java
+++ src/main/java/jp/falsystack/Theater.java
@@ -1,2 +1,22 @@
-package jp.falsystack;public class Theater {
+package jp.falsystack;
+
+public class Theater {
+
+  private final TicketSeller ticketSeller;
+
+  public Theater(TicketSeller ticketSeller) {
+    this.ticketSeller = ticketSeller;
+  }
+
+  public void enter(Audience audience) {
+    if (audience.getBag().hasInvitation()) {
+      Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+      audience.getBag().setTicket(ticket);
+      return;
+    }
+    Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+    audience.getBag().minusAmount(ticket.getFee());
+    ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+    audience.getBag().setTicket(ticket);
+  }
 }
--- src/main/java/jp/falsystack/TicketSeller.java
+++ src/main/java/jp/falsystack/TicketSeller.java
@@ -1,2 +1,15 @@
-package jp.falsystack;public class TicketSeller {
+package jp.falsystack;
+
+import lombok.Getter;
+
+public class TicketSeller {
+
+  @Getter
+  private final TicketOffice ticketOffice;
+
+  public TicketSeller(TicketOffice ticketOffice) {
+    this.ticketOffice = ticketOffice;
+  }
+
+
 }
