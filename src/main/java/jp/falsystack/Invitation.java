95f5d7da72898543b4e327671cb53999af8d29ec
falsystack <falsystack@gmail.com>
2023-03-15 21:00:36 +0900

    add Lombok

---
+++ src/main/java/jp/falsystack/Invitation.java
@@ -0,0 +1,9 @@
+package jp.falsystack;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+
+  private LocalDateTime when;
+
+}
---
+++ src/main/java/jp/falsystack/Ticket.java
@@ -0,0 +1,9 @@
+package jp.falsystack;
+
+public class Ticket {
+
+  private Long fee;
+
+
+
+}
