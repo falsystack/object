462ce1cfc3356f4578bffe04cc4eba5dab45bd84
falsystack <falsystack@gmail.com>
2023-03-16 21:22:28 +0900

    refactor(chap01): audienceのカプセル化

--- src/main/java/jp/falsystack/Audience.java
+++ src/main/java/jp/falsystack/Audience.java
@@ -4,12 +4,21 @@ import lombok.Getter;
 
 public class Audience {
 
-  @Getter
   private final Bag bag;
 
   public Audience(Bag bag) {
     this.bag = bag;
   }
 
+
+  public Long buy(Ticket ticket) {
+    if (bag.hasInvitation()) {
+      bag.setTicket(ticket);
+      return 0L;
+    }
+    bag.minusAmount(ticket.getFee());
+    bag.setTicket(ticket);
+    return ticket.getFee();
+  }
 
 }
