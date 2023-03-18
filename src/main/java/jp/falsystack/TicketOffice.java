1b2ee4fbff84ad3e5542f76e8ebd771a6b56e802
falsystack <falsystack@gmail.com>
2023-03-18 20:11:02 +0900

    refactor(chap01): TradeOff

--- src/main/java/jp/falsystack/TicketOffice.java
+++ src/main/java/jp/falsystack/TicketOffice.java
@@ -14,7 +14,7 @@ public class TicketOffice {
     this.tickets.addAll(Arrays.asList(tickets));
   }
 
-  public Ticket getTicket() {
+  private Ticket getTicket() {
     return tickets.remove(0);
   }
 
@@ -22,8 +22,12 @@ public class TicketOffice {
     this.amount -= amount;
   }
 
-  public void plusAmount(Long amount) {
+  private void plusAmount(Long amount) {
     this.amount += amount;
+  }
+
+  public void sellTicketTo(Audience audience) {
+    plusAmount(audience.buy(getTicket()));
   }
 
 }
