2cf8c6078987146b4182c32813c415a560562e49
falsystack <falsystack@gmail.com>
2023-03-22 00:36:25 +0900

    feat(chap02): implementation DiscountPolicy

---
+++ src/main/java/jp/falsystack/chap02/DiscountPolicy.java
@@ -0,0 +1,25 @@
+package jp.falsystack.chap02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class DiscountPolicy {
+
+  private List<DiscountCondition> conditions = new ArrayList<>();
+
+  public DiscountPolicy(DiscountCondition... conditions) {
+    this.conditions = Arrays.asList(conditions);
+  }
+
+  public Money caculateDiscountAmount(Screening screening) {
+    for (DiscountCondition each : conditions) {
+      if (each.isSatisfiedBy(screening)) {
+        return getDiscountAmount(screening);
+      }
+    }
+    return Money.ZERO;
+  }
+
+  abstract protected Money getDiscountAmount(Screening screening);
+}
